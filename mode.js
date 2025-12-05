d0acc4e676f2d6db7e6f1fb23cd3e59e287790bd
THARUNIKA SS <tharunika.ss2024cse@sece.ac.in>
2025-12-05 09:20:44 +0530

    Add files via upload

---
+++ mode.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    let body =
+    
+    if (body.style.backgroundColor === "black") {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    } else {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    }
+}
